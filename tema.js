948d0e59bfe66136495709f629786e2a921873b0
angie <amartinezsana@gmail.com>
2024-02-23 16:24:47 -0500

    hemos agregado un archivo

---
+++ tema.js
@@ -0,0 +1,71 @@
+// creacion de variables
+var x;
+let y;
+
+x = 5;
+y = 10;
+let z = x + y;
+console.log(z);
+
+//camelCase
+//PascalCase
+// snake_case
+
+var firstName = "Pepe";
+var lasteName = "Perez"; 
+
+//Operadores en JS
+var num1 = 10;
+var num2 = 7;
+var num3 = 3;
+
+let result = (num1 + num2)* num3
+
+//concatenar cadenas de caracteres
+let fullName =firstName + " " + lasteName;
+console.log (fullName);
+
+var a = 5;
+var b = "5"
+let c = a +b;
+console.log(c);
+
+/* JavaScrript case sensitive 
+ lastname  lastName */
+
+ //variables 4 formas
+ //automatica
+ //var -> 1995
+ //let
+ //const -> 2015 */
+
+ const NOMBRE_CONSTANTE = 15;
+ let myVariable;
+myVariable = "Hola";
+
+const PI = 3.14;
+//let person = "Pepe Perez";
+let carName;
+
+let person = "Pepe Perez", carName1 = "volvo", price = 200;
+// re-declaracion de variables
+var carName2 = "renault";
+var carName2;
+
+console.log(carName2);
+
+
+let carName3 = "BMW";
+//let carName3;
+
+//alcance de variables
+/*{
+    let abc = 2
+    console.log(abc);
+}*/
+
+var aprendiz = "juanita";
+{
+    console.log(aprendiz)
+}
+
